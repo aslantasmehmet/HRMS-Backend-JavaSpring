6dffeba36472217888cedb0c5878465b45a207bc
Mehmet ASLANTAŞ <aslantas-mehmet58@hotmail.com>
2022-05-17 12:15:42 +0300

    The project has been created. Layered architecture was created.

---
+++ src/main/java/kodlamaio/HRMS/dataAccess/abstracts/PositionDao.java
@@ -0,0 +1,9 @@
+package kodlamaio.HRMS.dataAccess.abstracts;
+
+import javax.swing.text.Position;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PositionDao extends JpaRepository<Position, Integer> {
+
+}
