8f5c73a0ac4e99190dbf2e05adb063091fe059f9
Mehmet ASLANTAŞ <aslantas-mehmet58@hotmail.com>
2022-06-09 14:20:00 +0300

    Employee , Registration scenarios were coded.

--- src/main/java/kodlamaio/HRMS/business/concretes/EmployeeManager.java
+++ src/main/java/kodlamaio/HRMS/business/concretes/EmployeeManager.java
@@ -1,14 +1,20 @@
 package kodlamaio.HRMS.business.concretes;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.springframework.stereotype.Service;
 
 import kodlamaio.HRMS.business.abstracts.EmployeeService;
+import kodlamaio.HRMS.core.utilities.business.BusinessRules;
+import kodlamaio.HRMS.core.utilities.business.validation.NationalityIdValidation;
 import kodlamaio.HRMS.core.utilities.result.DataResult;
+import kodlamaio.HRMS.core.utilities.result.ErrorResult;
 import kodlamaio.HRMS.core.utilities.result.Result;
 import kodlamaio.HRMS.core.utilities.result.SuccessDataResult;
 import kodlamaio.HRMS.core.utilities.result.SuccessResult;
+import kodlamaio.HRMS.dataAccess.abstracts.EmailVerificationDao;
 import kodlamaio.HRMS.dataAccess.abstracts.EmployeeDao;
 import kodlamaio.HRMS.entities.concretes.Employee;
 
@@ -16,10 +22,12 @@ import kodlamaio.HRMS.entities.concretes.Employee;
 public class EmployeeManager implements EmployeeService {
 	
 	private EmployeeDao employeeDao;
+	private EmailVerificationDao emailVerificationDao;
 
-	public EmployeeManager(EmployeeDao employeeDao) {
+	public EmployeeManager(EmployeeDao employeeDao,EmailVerificationDao emailVerificationDao) {
 		super();
 		this.employeeDao = employeeDao;
+		this.emailVerificationDao = emailVerificationDao;
 	}
 
 	@Override
@@ -30,9 +38,52 @@ public class EmployeeManager implements EmployeeService {
 
 	@Override
 	public Result add(Employee employee) {
+		var result = BusinessRules.run(
+				CheckIfTheEmailIsRegistered(employee),
+				CheckIfTheİdentificationNumber(employee),
+				isRealEmail(employee),
+				İdentificationNumberValidation(employee)
+				);
+		
+		if(result != null) {
+			return result;
+		}
+		
 		this.employeeDao.save(employee);
-		return new SuccessResult("Yeni İş Arayan Eklendi.");
+		return new SuccessResult("Kayıt Olma İşlemi Başarılı");
+	}
+	
+	private Result CheckIfTheEmailIsRegistered (Employee employee) {
+		if(employeeDao.findAllByEmail(employee.getEmail()).stream().count() !=0) {
+			return new ErrorResult("'" + employee.getEmail()+ "'"+ "Adresiyle daha önce Hesap Açılmış.");
+		}
+		return new SuccessResult();
 	}
 
+	private Result CheckIfTheİdentificationNumber(Employee employee) {
+		if(employeeDao.findAllByİdentificationNumber(employee.getIdentificationNumber()).stream().count() !=0) {
+			return new ErrorResult("'" + employee.getIdentificationNumber()+ "'" + "Bu Kimlik Numarası ile Daha önce Kayıt Olunmuştur.");
+		}
+		return new SuccessResult();
+	}
+	
+	
+	private Result isRealEmail (Employee employee) {
+		 
+		String regex = "^(.+)@(.+)$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher  = pattern.matcher(employee.getEmail());
+		if(!matcher.matches()) {
+			return new ErrorResult("Hatalı Email Adresi giridiniz.");
+			
+		}
+		return new SuccessResult();
+	}
+	private Result İdentificationNumberValidation(Employee employee) {
+		if(!NationalityIdValidation.isRealPerson(employee.getIdentificationNumber())) {
+			return new ErrorResult("Kimlik Doğrulanamadı");
+		}
+		return new SuccessResult();
+	}
 	
 }
--- src/main/java/kodlamaio/HRMS/dataAccess/abstracts/EmailVerificationDao.java
+++ src/main/java/kodlamaio/HRMS/dataAccess/abstracts/EmailVerificationDao.java
@@ -1,9 +1,12 @@
 package kodlamaio.HRMS.dataAccess.abstracts;
 
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import kodlamaio.HRMS.entities.concretes.EmailVerification;
 
 public interface EmailVerificationDao extends JpaRepository<EmailVerification, Integer>{
-
+	
+	
 }
