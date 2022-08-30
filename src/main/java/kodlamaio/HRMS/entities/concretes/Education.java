71b8347105f2a404987b004bbd7ea63b46e01609
Mehmet ASLANTAŞ <aslantas-mehmet58@hotmail.com>
2022-08-30 09:41:24 +0300

    The CV and cover letter section has been added to the system.

--- src/main/java/kodlamaio/HRMS/entities/concretes/Education.java
+++ src/main/java/kodlamaio/HRMS/entities/concretes/Education.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -51,6 +53,8 @@ public class Education {
 		return graduationDate.toString();
 	}
 	
-	
+	@ManyToOne
+	@JoinColumn(name="resume_id")
+	private Resume resume;
 
 }
