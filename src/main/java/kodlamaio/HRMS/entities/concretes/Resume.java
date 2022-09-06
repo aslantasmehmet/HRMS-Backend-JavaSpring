d4127c72d017c72ca4734c560b1c7827e4bdae56
Mehmet ASLANTAŞ <aslantas-mehmet58@hotmail.com>
2022-09-06 14:10:12 +0300

    The resume is included in the system

--- src/main/java/kodlamaio/HRMS/entities/concretes/Resume.java
+++ src/main/java/kodlamaio/HRMS/entities/concretes/Resume.java
@@ -1,6 +1,12 @@
 package kodlamaio.HRMS.entities.concretes;
 
+import java.time.LocalDate;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -19,7 +25,24 @@ import lombok.NoArgsConstructor;
 
 public class Resume {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="resume_id")
 	private int resumeId;
+	
+	@Column(name="resume_name")
+	private String resumeName;
+	
+	@Column(name="visibilty")
+	private String visibilty;
+	
+	@Column(name="creation_date")
+	private LocalDate creationDate;
+	
+	@Column(name="last_updated_date")
+	private LocalDate lastUpdatedDate;
+	
+	
 	
 
 }
